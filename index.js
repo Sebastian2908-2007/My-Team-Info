d32a0c7d0aacea64b262db7fceff4b01d7b45f7a
sebastian <sebastian bowen>
2021-03-27 23:07:17 -0600

    main logic need to switch branches

--- index.js
+++ index.js
@@ -2,4 +2,26 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 const {writeFile, copyFile} = require('./utils/generate-page');
-
+const inquirer = require("inquirer");
+ 
+const initManager = () => {
+    let manager;
+   return inquirer
+      .prompt([ 
+      {
+          type: "text",
+          name: 'name',
+          message: 'What is the team managers name?'
+    },
+    {
+     type: 'text',
+     name: 'employId',
+     message: 'What is the managers employee ID?'   
+    },
+      ])
+      .then(({name,employId,eMail,officeNum}) => {
+      manager = new Manager(name, employId)
+      console.log(manager);
+      });
+}
+  initManager();
